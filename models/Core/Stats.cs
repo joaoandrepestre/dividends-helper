856a774628e32c1ebf9b0a141825ba3b8e775214
João André Pestre <joaoandre.pestre@poli.ufrj.br>
2023-07-26 11:56:50 -0300

    fix: division by 0 on Stats

--- models/Core/Stats.cs
+++ models/Core/Stats.cs
@@ -6,20 +6,20 @@ public class Stats {
     public int Count => Values.Length;
 
     public decimal Total => Values.Sum();
-    public decimal Average => Total / Count;
+    public decimal Average => Count == 0 ? 0 : Total / Count;
 
-    public decimal Median => Values
+    public decimal Median => Count == 0 ? 0 : Values
         .OrderBy(v => v)
         .Skip((Count - 1) / 2)
         .Take(2 - Count % 2)
         .Average();
 
-    public decimal Mode => Values
+    public decimal Mode => Count == 0 ? 0 : Values
         .GroupBy(v => v)
         .OrderByDescending(g => g.Count())
         .First().Key;
 
-    public decimal Variance => Values.Sum(v => (v - Average) * (v - Average)) / Count;
+    public decimal Variance => Count == 0 ? 0 : Values.Sum(v => (v - Average) * (v - Average)) / Count;
     public decimal StandardDeviation => (decimal) Math.Sqrt((double) Variance);
 
     public Stats(IEnumerable<decimal> values)
